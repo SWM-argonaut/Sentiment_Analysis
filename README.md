9b00595068f1036685811046d64be11593729dc9
quartzes@kaist.ac.kr <elwerks0364for>
2021-07-05 15:05:04 +0900

    Sentiment Analysis Init

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Sentiment_Analysis
 
-댓글 감정 분석기
+## 주의사항
+
+- Pororo 최신 버전에서는 Sentiment Analysis 관련 버그가 있습니다. (model args 누락) 
+- 해당 분석 기능을 사용하시려면 다음 사항을 수정하셔야 합니다.
+
+---
+
+- models > brainbert > BrainRoBERTa.py
+- predict_output()
+
+<pre><code>assert self.args.task == "sentence_prediction" ## 주석처리</code></pre>
+<pre><code>self.args.regression_target ## 원하는 결과 type에 맞게 boolean으로 치환</code></pre>
+
+---
+
+## 사용법
+
+1. input 폴더에 지정된 형식의 json 파일 추가
+2. input_list.py 에 추가된 json 파일명 기입
+3. main.py 실행
+
+---
+
+## 사용 라이브러리
+
+- PORORO (https://github.com/kakaobrain/pororo)
---
+++ main.py
@@ -0,0 +1,45 @@
+import json
+
+from pororo import Pororo
+from input_list import file_list
+
+def generate_sentences(file_path):
+    sentences = []
+
+    with open(file_path, encoding='utf-8') as json_file:
+        json_data = json.load(json_file)
+        json_comments_data = json_data["comments"]
+
+        for comments in json_comments_data:
+            sentence = ""
+
+            if comments["headline"] != None: sentence += comments["headline"] + " "
+            if comments["review"] != None: sentence += comments["review"]
+
+            sentences.append(sentence)
+
+    return sentences
+
+def sentiment_analysis(sentences):
+    sa = Pororo(task="sentiment", model="brainbert.base.ko.shopping", lang="ko")
+
+    positive_count = 0
+    negative_count = 0
+
+    for sentence in sentences:
+        if sa(sentence) == "Positive":
+            positive_count += 1
+        else:
+            negative_count += 1
+    
+    return float(positive_count) / float(positive_count + negative_count)
+
+if __name__ == "__main__":
+    result = open("result/result.txt", 'w')
+
+    for file_name in file_list:
+        sentences = generate_sentences("input/" + file_name + ".json")
+        positive_percent = sentiment_analysis(sentences)
+        result.write(file_name + ": %f\n" % positive_percent)
+
+    result.close()
